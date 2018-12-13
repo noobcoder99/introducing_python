0218ab3672afc839f359b895024f38a2bb5bcf30
john <john@somemail.com>
2018-12-14 07:50:55 +1000

    new

---
+++ a_draft/lists.py
@@ -0,0 +1,5 @@
+small_birds=['humming', 'finch']
+big_birds=['emu', 't-rex']
+all_birds=[small_birds,big_birds]
+all_birds[0]
+print(all_birds)
---
+++ a_draft/poam.py
@@ -0,0 +1,2 @@
+poam2= '"I dont like thee to be or not to be,\n, will ghost so another radio show"'
+print(poam2)
