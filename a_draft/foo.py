96a7c617480d07dd41f80700aaa27a4e18833c0b
fmc <fmc@skynet3.local>
2019-06-15 10:46:36 +1000

    new

--- a_draft/foo.py
+++ a_draft/foo.py
@@ -1,8 +1,3 @@
-while True:
-    value = input("Interger, please [q to quit]:")
-    if value == 'q': #quit
-        break
-    number = int(value)
-    if number % 2 == 0: #an even number
-        continue
-    print(number, "squared is", number*number)
+word = 'ghost'
+for letter in word:
+    print(letter)
